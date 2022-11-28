13972a438dcb9769c16ca30773aaaebce8a07d5b
B S Abhishek <67589384+Abhishekbestha@users.noreply.github.com>
2022-11-28 23:09:12 +0530

    Update Sucess.java

--- src/main/java/com/mycompany/resume/Sucess.java
+++ src/main/java/com/mycompany/resume/Sucess.java
@@ -4,14 +4,14 @@
  */
 package com.mycompany.resume;
 
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
